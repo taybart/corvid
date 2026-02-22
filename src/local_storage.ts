a3958ee21500e9cb7672c19fd0191de2e62f0f81
taylor <taybart@gmail.com>
2026-02-21 20:16:25 -0700

    fix: set localstorage before publishing update

--- src/local_storage.ts
+++ src/local_storage.ts
@@ -27,21 +27,21 @@ export function update(
 ) {
   const prev = get(key)
   const value = update(prev)
+  localStorage.setItem(key, value)
   if (prev !== value || broadcast) {
     const event = new CustomEvent('@corvid/ls-update', {
       detail: { key, value },
     })
     document.dispatchEvent(event)
   }
-  localStorage.setItem(key, value)
 }
 
 // set: if key is an object, setObj will be called with value passed as a prefix
 export function set(key: string, value: any, broadcast: boolean = false) {
-  // if (typeof key === 'object') {
-  //   setObj(key, value, broadcast)
-  //   return
-  // }
+  if (typeof value === 'object') {
+    value = JSON.stringify(value)
+  }
+  localStorage.setItem(key, value)
   const prev = get(key)
   if (prev !== value || broadcast) {
     const event = new CustomEvent('@corvid/ls-update', {
@@ -49,59 +49,7 @@ export function set(key: string, value: any, broadcast: boolean = false) {
     })
     document.dispatchEvent(event)
   }
-  if (typeof value === 'object') {
-    value = JSON.stringify(value)
-  }
-  localStorage.setItem(key, value)
 }
-// FIXME: i think this function is confusing
-export function setObj(
-  update: object,
-  prefix?: string,
-  broadcast: boolean = false,
-) {
-  const flatten = (ob: any) => {
-    const ret: Record<string, any> = {}
-
-    for (let i in ob) {
-      if (!ob.hasOwnProperty(i)) continue
-
-      if (typeof ob[i] == 'object' && ob[i] !== null) {
-        const flat = flatten(ob[i])
-        for (let x in flat) {
-          if (!flat.hasOwnProperty(x)) continue
-          ret[`${i}.${x}`] = flat[x]
-        }
-      } else {
-        ret[i] = ob[i]
-      }
-    }
-    return ret
-  }
-  for (let [k, v] of Object.entries(flatten(update))) {
-    let key = k
-    if (prefix) {
-      key = `${prefix}.${k}`
-    }
-    set(key, v, broadcast)
-  }
-}
-// export function listen(
-//   key: string,
-//   cb: (update: { key: string; value: any }) => void | el,
-// ) {
-//   document.addEventListener('@corvid/ls-update', (ev: any) => {
-//     if (ev.detail.key === key || key === '*') {
-//       if (cb instanceof el) {
-//         if (ev.detail.key === key) {
-//           cb.content(ev.detail.value)
-//         }
-//         return
-//       }
-//       cb({ key: ev.detail.key, value: ev.detail.value })
-//     }
-//   })
-// }
 
 const listeners = new Map<string, Map<Function, Function>>()
 
